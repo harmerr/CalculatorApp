f89bba32ffe4dd4b70f255b699ad4c276de572f4
harmerr <101577717+harmerr@users.noreply.github.com>
2022-04-10 23:50:27 -0400

    finished js file

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const userInput = document.querySelectorAll('.inputElements')
-let lastInput
-let totalInput = document.getElementById('#calculatorInput')
+const equals = document.querySelector(".evaluate")
+const clearInput = document.querySelector(".clear")
+const display = document.querySelector("#calculatorInput")
+let totalInput = ''
+let lastInput = ''
 
 userInput.forEach((input, i) => {
     input.addEventListener('click', () => {
         lastInput = input.innerHTML
         console.log(lastInput)
-        totalInput = totalInput.append(lastInput)
+        totalInput += lastInput
+        document.querySelector("#calculatorInput").innerHTML = totalInput
     })
 })
 
+equals.addEventListener('click', () => {
+    display.innerHTML = eval(totalInput)
+    totalInput = display.innerHTML
+})
 
-console.log(totalInput)
+clearInput.addEventListener('click', () => {
+    totalInput = ''
+    display.innerHTML = ''
+})
+
+function clearAll() {
+    totalInput = ''
+    display.innerHTML = ''
+}
